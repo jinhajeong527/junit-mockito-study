f660259678d6516daebd68ad2385f4f3a5841132
정진하 <jinha4ever@gmail.com>
2022-12-09 21:29:30 +0900

    기본 파일 커밋

---
+++ mockito-course/src/main/java/com/in28minutes/mockito/MockitoCourseApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoCourseApplication.class, args);
+	}
+
+}
