fa7bc527c86b29e0d24995852bc02a0ba7e1636b
정진하 <jinha4ever@gmail.com>
2022-12-10 11:17:46 +0900

    argument capture using ArgumentCaptor

--- mockito-course/src/test/java/com/in28minutes/mockito/business/TodoBusinessImplMockTest.java
+++ mockito-course/src/test/java/com/in28minutes/mockito/business/TodoBusinessImplMockTest.java
@@ -3,13 +3,18 @@ package com.in28minutes.mockito.business;
 import com.in28minutes.mockito.data.api.TodoService;
 import com.in28minutes.mockito.data.api.TodoServiceStub;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 public class TodoBusinessImplMockTest {
@@ -90,10 +95,36 @@ public class TodoBusinessImplMockTest {
 
         // Then
         verify(todoServiceMock, atMost(1)).deleteTodo("Learn How to Use IntelliJ IDE");
+        // More Readable
+        then(todoServiceMock).should().deleteTodo("Learn How to Use IntelliJ IDE");
         verify(todoServiceMock, times(1)).deleteTodo("Learn Junit"); // 한번만 호출되어야 한다.
         verify(todoServiceMock, atLeast(1)).deleteTodo("Learn Java Script");
 
         verify(todoServiceMock, never()).deleteTodo("Learn Spring MVC");
+        // More Readable
+        then(todoServiceMock).should(never()).deleteTodo("Learn Spring MVC");
+    }
+    @Test
+    public void testDeleteTodosNotRelatedToSpring_usingBDD_argumentCapture() {
+        // Declare Argument Captor
+        ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+        // Define Argument captor on specific method call
+        // Capture the argument
+
+        // Given : Set Up
+        TodoService todoServiceMock = mock(TodoService.class);
+        List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring Data JPA", "Learn Spring Test",
+                "Learn How to Use IntelliJ IDE", "Learn Junit", "Learn Java Script");
+        // given(), willReturn()
+        given(todoServiceMock.retrieveTodos("DummyUser")).willReturn(todos);
+        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+
+        // When
+        todoBusinessImpl.deleteTodosNotRelatedToSpring("DummyUser");
+
+        // Then
+        then(todoServiceMock).should(times(3)).deleteTodo(stringArgumentCaptor.capture());
+        assertThat(stringArgumentCaptor.getAllValues().size(), is(3));
     }
 
 }
