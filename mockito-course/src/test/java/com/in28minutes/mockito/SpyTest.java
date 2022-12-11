c860df7614a2ab88e27949de7a441e052c455c09
정진하 <jinha4ever@gmail.com>
2022-12-11 10:37:56 +0900

    Mockito Spy 실습

--- mockito-course/src/test/java/com/in28minutes/mockito/SpyTest.java
+++ mockito-course/src/test/java/com/in28minutes/mockito/SpyTest.java
@@ -8,21 +8,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class SpyTest {
 
     @Test
     public void test() {
-        List arrayListMock = mock(ArrayList.class);
-        // mocks return default value
-        assertEquals(0, arrayListMock.size());
-        // 이렇게 한다고 실제 ArrayList의 사이즈에 영향을 미치지 않는다.
-        // Mock을 쓸 때는 ArrayList의 어떤 implementation에도 관심이 없다.
-        arrayListMock.add("Test Dummy Data");
-        // stub() 메서드 대신에 when()을 사용할 수 있다.
-        when(arrayListMock.size()).thenReturn(5);
-        assertEquals(5, arrayListMock.size());
+        // Creating a real arrayList
+        // 실제로 stubbing 할 수 있다.
+        List arrayListSpy = spy(ArrayList.class);
+        when(arrayListSpy.size()).thenReturn(5);
+        // size() method만 오버라이드 하게 된다.
+        assertEquals(5, arrayListSpy.size());
+        arrayListSpy.add("Dummy"); // Has this really happen? yes
+        verify(arrayListSpy).add("Dummy"); // clear() call never get called right? no
+        verify(arrayListSpy, never()).clear();
+        // 스파이는 바뀌길 원하는 부분을 제외하고는 실제 액션은 그대로 발생하게 두고,
+        // 그것을 트랙킹 할 수 있게 해준다.
+        // 잘 구성되었다면 스파이를 꼭 쓸 필요 없고 목을 쓰는 것이 더 좋다
+        // 레거시 프로젝트라면 spy가 필요할 수가 있다.
     }
 }
